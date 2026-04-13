2a95763d02233f9b2b160ea14de107e088426d29
Mihail Shterev <93082689+mihailshterev@users.noreply.github.com>
2026-04-13 23:08:35 +0300

    remove form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,23 +2,20 @@ import { motion } from 'framer-motion'
 
 export const ContactForm = ({animationControls}) => {
   return (
-    <motion.form action="https://formsubmit.co/74fe684c8f1aa031c582f96066ec3486" method="POST" className="contact-form"
-        variants={{
-          hidden: {opacity: 0},
-          visible: {opacity: 1}
-        }}
-        initial="hidden"
-        animate={animationControls}
-        transition={{duration: 1}}>
-        <h2>Contact Me</h2>
-        <div className="input-container">
-            <input type="text" name='name' className='contact-input' placeholder='Name' required/>
-            <input type="email" name='email' className='contact-input' placeholder='Email' required/>
-            <input type="hidden" name="_captcha" value="false"></input>
-            <input type="hidden" name="_subject" value="New portfolio submission!"></input>
-        </div>
-        <textarea type="text" name='message' className='contact-input-msg' placeholder='Message' required/>
-        <button type='submit'>Send</button>
-    </motion.form>
+    <motion.div 
+      className="contact-form"
+      variants={{
+        hidden: { opacity: 0, y: 10 },
+        visible: { opacity: 1, y: 0 }
+      }}
+      initial="hidden"
+      animate={animationControls}
+      transition={{ duration: 1 }}
+    >
+      <h2>Let's Connect</h2>
+      <p>
+        Check out my links below!
+      </p>
+    </motion.div>
   )
 }
